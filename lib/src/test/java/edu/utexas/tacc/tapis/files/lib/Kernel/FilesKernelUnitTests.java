a188863aa54d17496782c53f889de6a4694e7365
Anagha Jamthe <ajamthe@wireless-10-146-85-131.public.utexas.edu>
2019-10-30 08:28:34 -0500

    removed docker-compose from resources

--- lib/src/test/java/edu/utexas/tacc/tapis/files/lib/Kernel/FilesKernelUnitTests.java
+++ lib/src/test/java/edu/utexas/tacc/tapis/files/lib/Kernel/FilesKernelUnitTests.java
@@ -1,5 +1,84 @@
 package edu.utexas.tacc.tapis.files.lib.Kernel;
 
-public class FilesKernelUnitTests {
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
+@Test(groups = { "unit" })
+public class FilesKernelUnitTests {
+/*
+@Test
+public void Test() {
+	final String uuid = "783738383";
+    final String methodName = "test";
+    final Boolean expected = false;
+    final Boolean actual = checkUUIDBlank(uuid, methodName);
+    Assert.assertEquals(actual, expected);
+
+	
+}
+
+@Test
+public void Test() {
+	
+	
+}
+@Test
+public void Test() {
+	
+	
+}
+
+@Test
+public void Test() {
+	
+	
+}
+
+@Test
+public void Test() {
+	
+	
+}
+
+@Test
+public void Test() {
+	
+	
+}
+
+@Test
+public void Test() {
+	
+	
+}
+
+@Test
+public void Test() {
+	
+	
+}
+
+@Test
+public void Test() {
+	
+	
+}
+
+@Test
+public void Test() {
+	
+	
+}
+
+@Test
+public void Test() {
+	
+	
+}
+@Test
+public void Test() {
+	
+	
+}
+*/
 }
