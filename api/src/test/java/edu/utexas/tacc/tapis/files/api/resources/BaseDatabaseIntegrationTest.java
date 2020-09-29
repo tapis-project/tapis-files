926c7d8cc70e02ff0e818d7b91a11ce27f72b365
Joe Meiring <jmeiring@tacc.utexas.edu>
2020-09-29 15:50:05 -0500

    fix for creating a test bucket if it dne for integration tests

--- api/src/test/java/edu/utexas/tacc/tapis/files/api/resources/BaseDatabaseIntegrationTest.java
+++ api/src/test/java/edu/utexas/tacc/tapis/files/api/resources/BaseDatabaseIntegrationTest.java
@@ -5,6 +5,17 @@ import org.glassfish.jersey.test.JerseyTestNg;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.BucketAlreadyOwnedByYouException;
+import software.amazon.awssdk.services.s3.model.CreateBucketConfiguration;
+import software.amazon.awssdk.services.s3.model.CreateBucketRequest;
+
+import java.net.URI;
+
 
 @Test(groups={"integration"})
 public abstract class BaseDatabaseIntegrationTest extends JerseyTestNg.ContainerPerClassTest {
@@ -16,6 +27,35 @@ public abstract class BaseDatabaseIntegrationTest extends JerseyTestNg.Container
             .load();
         flyway.clean();
         flyway.migrate();
+    }
+
+    @BeforeTest
+    public void createTestBucket() {
+        Region region = Region.US_WEST_2;
+        String bucket = "test";
+        AwsCredentials credentials = AwsBasicCredentials.create(
+            "user",
+            "password"
+        );
+        S3Client s3 = S3Client.builder()
+            .region(region)
+            .credentialsProvider(StaticCredentialsProvider.create(credentials))
+            .endpointOverride(URI.create("http://localhost:9000"))
+            .build();
+
+        CreateBucketRequest createBucketRequest = CreateBucketRequest
+            .builder()
+            .bucket(bucket)
+            .createBucketConfiguration(CreateBucketConfiguration.builder()
+                .locationConstraint(region.id())
+                .build())
+            .build();
+
+        try {
+            s3.createBucket(createBucketRequest);
+        } catch (BucketAlreadyOwnedByYouException ex) {
+
+        }
     }
 
 
