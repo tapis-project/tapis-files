b208c1967dd0f7f6543d51b9c5b6759aa6f569f9
jmeiring <jmeiring@tacc.utexas.edu>
2021-08-19 12:08:13 -0500

    irods docker-compose and initial work on data client

--- lib/src/main/java/edu/utexas/tacc/tapis/files/lib/clients/IrodsDataClient.java
+++ lib/src/main/java/edu/utexas/tacc/tapis/files/lib/clients/IrodsDataClient.java
@@ -1,39 +1,56 @@
 package edu.utexas.tacc.tapis.files.lib.clients;
 
 import edu.utexas.tacc.tapis.files.lib.models.FileInfo;
+import edu.utexas.tacc.tapis.files.lib.utils.Utils;
 import edu.utexas.tacc.tapis.systems.client.gen.model.TapisSystem;
+import org.apache.commons.io.FilenameUtils;
+import org.irods.jargon.core.connection.AuthScheme;
+import org.irods.jargon.core.connection.IRODSAccount;
+import org.irods.jargon.core.exception.JargonException;
+import org.irods.jargon.core.pub.IRODSAccessObjectFactory;
+import org.irods.jargon.core.pub.IRODSFileSystem;
+import org.irods.jargon.core.pub.io.IRODSFileFactory;
+import org.irods.jargon.core.utils.MiscIRODSUtils;
 import org.jetbrains.annotations.NotNull;
 
 import javax.ws.rs.NotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 public class IrodsDataClient implements IRemoteDataClient {
 
-    private String oboTenantId;
-    private String oboUsername;
-    private TapisSystem system;
+    private final String oboTenantId;
+    private final String oboUsername;
+    private final TapisSystem system;
+    private final String irodsZone;
+    private final String homeDir;
+    private static final String DEFAULT_RESC = "demoResc";
 
     public IrodsDataClient(@NotNull String oboTenantId, @NotNull String oboUsername, @NotNull TapisSystem system) {
         this.oboTenantId = oboTenantId;
         this.oboUsername = oboUsername;
         this.system = system;
+        Path tmpPath = Paths.get(system.getRootDir());
+        this.irodsZone = tmpPath.getRoot().toString();
+        this.homeDir = Paths.get(irodsZone, "home", oboUsername).toString();
     }
 
     @Override
     public String getOboTenant() {
-        return null;
+        return oboTenantId;
     }
 
     @Override
     public String getOboUser() {
-        return null;
+        return oboUsername;
     }
 
     @Override
     public String getSystemId() {
-        return null;
+        return system.getId();
     }
 
     @Override
@@ -53,6 +70,32 @@ public class IrodsDataClient implements IRemoteDataClient {
 
     @Override
     public void mkdir(@NotNull String remotePath) throws IOException, NotFoundException {
+        String cleanedPath = FilenameUtils.normalize(remotePath);
+        IRODSAccessObjectFactory accessObjectFactory;
+        IRODSFileSystem irodsFileSystem;
+        IRODSAccount account;
+        IRODSFileFactory fileFactory;
+        try {
+           irodsFileSystem = IRODSFileSystem.instance();
+           account = IRODSAccount.instance(
+                system.getHost(),
+                system.getPort(),
+                oboUsername,
+                system.getAuthnCredential().getPassword(),
+                homeDir,
+                irodsZone,
+                DEFAULT_RESC,
+                AuthScheme.STANDARD
+            );
+           accessObjectFactory = irodsFileSystem.getIRODSAccessObjectFactory();
+           fileFactory = accessObjectFactory.getIRODSFileFactory(account);
+        } catch (JargonException ex) {
+            String msg = Utils.getMsg("FILES_IRODS_ERROR", oboTenantId, "", oboTenantId, oboUsername);
+            throw new IOException(msg, ex);
+        }
+
+
+
 
     }
 
