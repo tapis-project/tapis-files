fad757dd4cd7fe2d66ee8919d75a8d21b1c5964e
Steve Black <scblack@tacc.utexas.edu>
2021-11-18 12:25:35 -0600

    Update FilesApplication with Systems/Apps conventions and code.

--- api/src/main/java/edu/utexas/tacc/tapis/files/api/FilesApplication.java
+++ api/src/main/java/edu/utexas/tacc/tapis/files/api/FilesApplication.java
@@ -1,5 +1,7 @@
 package edu.utexas.tacc.tapis.files.api;
 
+import edu.utexas.tacc.tapis.files.api.providers.FilePermissionsAuthz;
+import edu.utexas.tacc.tapis.files.api.providers.FilesExceptionMapper;
 import edu.utexas.tacc.tapis.files.lib.caches.FilePermsCache;
 import edu.utexas.tacc.tapis.files.lib.caches.SystemsCache;
 import edu.utexas.tacc.tapis.files.lib.factories.ServiceContextFactory;
@@ -10,6 +12,7 @@ import edu.utexas.tacc.tapis.files.lib.services.IFileOpsService;
 import edu.utexas.tacc.tapis.files.lib.providers.ServiceClientsFactory;
 import edu.utexas.tacc.tapis.files.api.resources.*;
 import edu.utexas.tacc.tapis.files.lib.services.IFileUtilsService;
+import edu.utexas.tacc.tapis.shared.TapisConstants;
 import edu.utexas.tacc.tapis.shared.security.ServiceClients;
 import edu.utexas.tacc.tapis.shared.security.ServiceContext;
 import edu.utexas.tacc.tapis.files.lib.caches.SSHConnectionCache;
@@ -19,24 +22,24 @@ import edu.utexas.tacc.tapis.files.lib.config.RuntimeSettings;
 import edu.utexas.tacc.tapis.files.lib.dao.transfers.FileTransfersDAO;
 import edu.utexas.tacc.tapis.files.lib.services.TransfersService;
 import edu.utexas.tacc.tapis.shared.ssh.apache.SSHConnection;
+import edu.utexas.tacc.tapis.shared.utils.TapisUtils;
 import edu.utexas.tacc.tapis.sharedapi.jaxrs.filters.JWTValidateRequestFilter;
+import edu.utexas.tacc.tapis.sharedapi.providers.ObjectMapperContextResolver;
 import edu.utexas.tacc.tapis.shared.security.TenantManager;
-import io.swagger.v3.jaxrs2.integration.resources.OpenApiResource;
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Contact;
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.info.License;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import io.swagger.v3.oas.annotations.servers.Server;
-import io.swagger.v3.oas.annotations.tags.Tag;
+import edu.utexas.tacc.tapis.sharedapi.providers.ValidationExceptionMapper;
+import org.apache.commons.lang3.StringUtils;
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.grizzly.http.server.NetworkListener;
 import org.glassfish.grizzly.nio.transport.TCPNIOTransport;
 import org.glassfish.grizzly.threadpool.ThreadPoolConfig;
+import org.glassfish.hk2.api.ServiceLocator;
 import org.glassfish.hk2.utilities.binding.AbstractBinder;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.glassfish.jersey.internal.inject.InjectionManager;
+import org.glassfish.jersey.jackson.JacksonFeature;
+import org.glassfish.jersey.media.multipart.MultiPartFeature;
+import org.glassfish.jersey.server.ApplicationHandler;
+import org.glassfish.jersey.server.ResourceConfig;
 
 import javax.inject.Singleton;
 import javax.ws.rs.ApplicationPath;
@@ -45,7 +48,7 @@ import java.util.Collection;
 import java.util.concurrent.TimeUnit;
 
 
-/* TODO: Copied from Apps
+/*
  * Main startup class for the web application. Uses Jersey and Grizzly frameworks.
  *   Performs setup for HK2 dependency injection.
  *   Registers packages and features for Jersey.
@@ -57,78 +60,91 @@ import java.util.concurrent.TimeUnit;
  *
  * The path here is appended to the context root and is configured to work when invoked in a standalone
  * container (command line) and in an IDE (such as eclipse).
- * ApplicationPath set to "/" since each resource class includes "/v3/apps" in the
- *     path set at the class level. See AppResource.java, PermsResource.java, etc.
+ * ApplicationPath set to "/" since each resource class includes "/v3/files" in the
+ *     path set at the class level. See OperationsApiResource.java, PermissionsApiResource.java, etc.
  *     This has been found to be a more robust scheme for keeping startup working for both
  *     running in an IDE and standalone.
+ *
+ * For all logging use println or similar so we do not have a dependency on a logging subsystem.
  */
-// TODO (from Apps) @ApplicationPath("/")
-// The path here is appended to the context root and
-// is configured to work when invoked in a standalone 
-// container (command line) and in an IDE (eclipse).
-@OpenAPIDefinition(
-        info = @Info(
-                title = "Tapis Files API",
-                version = "0.0",
-                description = "My API",
-                license = @License(name = "Apache 2.0", url = "http://foo.bar"),
-                contact = @Contact(url = "http://tacc.utexas.edu", name = "CicSupport", email = "cicsupport@tacc.utexas.edu")
-        ),
-        tags = {
-                @Tag(name = "file operations"),
-                @Tag(name = "share"),
-                @Tag(name = "permissions"),
-                @Tag(name = "transfers")
-        },
-        security = {
-                @SecurityRequirement(name = "Bearer"),
-        },
-        servers = {
-                @Server(
-                        description = "localhost",
-                        url = "http://localhost:8080/"
-                ),
-                @Server(
-                        description = "development",
-                        url = "https://dev.develop.tapis.io"
-                )
-        }
-)
-@ApplicationPath("v3/files")
-public class FilesApplication extends BaseResourceConfig {
-    /**
-     * BaseResourceConfig has all the extra jersey filters and our
-     * custom ones for JWT validation and AuthZ
-     */
-    private static final Logger log = LoggerFactory.getLogger(FilesApplication.class);
-    private IRuntimeConfig runtimeConfig;
+@ApplicationPath("/")
+public class FilesApplication extends ResourceConfig
+{
+  // We must be running on a specific site and this will never change
+  private static String siteId;
+  public static String getSiteId() {return siteId;}
+  private static String siteAdminTenantId;
+  public static String getSiteAdminTenantId() {return siteAdminTenantId;}
 
-    public FilesApplication() {
-        super();
+  private IRuntimeConfig runtimeConfig;
 
-        runtimeConfig = RuntimeSettings.get();
-        TenantManager tenantManager = TenantManager.getInstance(runtimeConfig.getTenantsServiceURL());
-        tenantManager.getTenants();
-        JWTValidateRequestFilter.setSiteId(runtimeConfig.getSiteId());
-        JWTValidateRequestFilter.setService("files");
-        SSHConnection.setLocalNodeName(runtimeConfig.getHostName());
-        //JWT validation
-        register(JWTValidateRequestFilter.class);
+  public FilesApplication()
+  {
+    super();
+    // Log our existence.
+    // Output version information on startup
+    System.out.println("**** Starting Files Service. Version: " + TapisUtils.getTapisFullVersion() + " ****");
 
-        //Our APIs
-        register(ContentApiResource.class);
-        register(TransfersApiResource.class);
-        register(PermissionsApiResource.class);
-        register(ShareApiResource.class);
-        register(HealthApiResource.class);
-        register(OperationsApiResource.class);
-        register(UtilsLinuxApiResource.class);
+    // TODO: Some of these class registrations are also in BaseResourceConfig but cannot delete
+    //       BaseResourceConfig yet because it is used in 5 test classes. Review Systems and Apps code to see if
+    //       BaseResourceConfig can be deleted or if these registrations should be removed and the Base used instead.
+    // Needed for properly returning timestamps
+    // Also allows for setting a breakpoint when response is being constructed.
+    register(ObjectMapperContextResolver.class);
 
-        //OpenAPI jazz
-        register(OpenApiResource.class);
+    // Need this for some reason for multipart forms/ uploads
+    register(MultiPartFeature.class);
+    // Serialization
+    register(JacksonFeature.class);
+    // ExceptionMappers, need both because ValidationMapper is a custom Jersey thing and
+    // can't be implemented in a generic mapper
+    register(FilesExceptionMapper.class);
+    register(ValidationExceptionMapper.class);
 
-        //For dependency injection into the Resource classes for testing.
-        register(new AbstractBinder() {
+    // AuthZ filters
+    register(FilePermissionsAuthz.class);
+
+    //JWT validation
+    register(JWTValidateRequestFilter.class);
+
+    //Our APIs
+    register(ContentApiResource.class);
+    register(TransfersApiResource.class);
+    register(PermissionsApiResource.class);
+    register(ShareApiResource.class);
+    register(HealthApiResource.class);
+    register(OperationsApiResource.class);
+    register(UtilsLinuxApiResource.class);
+
+    //OpenAPI jazz
+//    register(OpenApiResource.class);
+
+    // We specify what packages JAX-RS should recursively scan to find annotations. By setting the value to the
+    // top-level directory in all projects, we can use JAX-RS annotations in any tapis class. In particular, the filter
+    // classes in tapis-shared-api will be discovered whenever that project is included as a maven dependency.
+    packages("edu.utexas.tacc.tapis");
+
+    // Set the application name. Note that this has no impact on base URL
+    setApplicationName(TapisConstants.SERVICE_NAME_FILES);
+
+    // Perform remaining init steps in try block so we can print a fatal error message if something goes wrong.
+    try {
+      runtimeConfig = RuntimeSettings.get();
+      siteId = runtimeConfig.getSiteId();
+
+      String url = runtimeConfig.getTenantsServiceURL();
+      TenantManager tenantManager = TenantManager.getInstance(url);
+      tenantManager.getTenants();
+      // Set admin tenant also, needed when building a client for calling other services (such as SK) as ourselves.
+      siteAdminTenantId = tenantManager.getSiteAdminTenantId(siteId);
+
+      JWTValidateRequestFilter.setSiteId(siteId);
+      JWTValidateRequestFilter.setService(TapisConstants.SERVICE_NAME_FILES);
+      SSHConnection.setLocalNodeName(runtimeConfig.getHostName());
+
+      // Initialize bindings for HK2 dependency injection
+      //For dependency injection into the Resource classes for testing.
+      register(new AbstractBinder() {
             @Override
             protected void configure() {
                 bind(new SSHConnectionCache(5, TimeUnit.MINUTES)).to(SSHConnectionCache.class);
@@ -143,30 +159,60 @@ public class FilesApplication extends BaseResourceConfig {
                 bindFactory(ServiceContextFactory.class).to(ServiceContext.class).in(Singleton.class);
                 bind(FileOpsService.class).to(IFileOpsService.class).in(Singleton.class);
                 bind(FileUtilsService.class).to(IFileUtilsService.class).in(Singleton.class);
-            }
+          }
         });
-		setApplicationName("files");
+    } catch (Exception e) {
+      // This is a fatal error
+      System.out.println("**** FAILURE TO INITIALIZE: Tapis Files Service ****");
+      e.printStackTrace();
+      throw e;
     }
+  }
 
   /**
    * Embedded Grizzly HTTP server
    */
   public static void main(String[] args) throws Exception {
-        final URI BASE_URI = URI.create("http://0.0.0.0:8080/");
-        FilesApplication config = new FilesApplication();
+    // If TAPIS_SERVICE_PORT set in env then use it.
+    // Useful for starting service locally on a busy system where 8080 may not be available.
+    String servicePort = System.getenv("TAPIS_SERVICE_PORT");
+    if (StringUtils.isBlank(servicePort)) servicePort = "8080";
 
-        final HttpServer server = GrizzlyHttpServerFactory.createHttpServer(BASE_URI, config, false);
-        Collection<NetworkListener> listeners = server.getListeners();
-        for(NetworkListener listener : listeners) {
-            log.info(listener.toString());
-            final TCPNIOTransport transport = listener.getTransport();
-            transport.setKeepAlive(true);
-            transport.setWriteTimeout(0, TimeUnit.MINUTES);
+    // Set base protocol and port. If mainly running in k8s this may not need to be configurable.
+    final URI baseUri = URI.create("http://0.0.0.0:" + servicePort + "/");
 
-            final ThreadPoolConfig tpc = transport.getWorkerThreadPoolConfig();
-            tpc.setQueueLimit(-1).setCorePoolSize(100).setMaxPoolSize(100);
-        }
+    // Initialize the application container
+    FilesApplication config = new FilesApplication();
 
-      server.start();
+    // TODO/TBD - adapt from Apps to Files
+    //  Initialize the service
+    // In order to instantiate our service class using HK2 we need to create an application handler
+    //   which allows us to get an injection manager which is used to get a locator.
+    //   The locator is used get classes that have been registered using AbstractBinder.
+    // NOTE: As of Jersey 2.26 dependency injection was abstracted out to make it easier to use DI frameworks
+    //       other than HK2, although finding docs and examples on how to do so seems difficult.
+//    ApplicationHandler handler = new ApplicationHandler(config);
+//    InjectionManager im = handler.getInjectionManager();
+//    ServiceLocator locator = im.getInstance(ServiceLocator.class);
+//    AppsServiceImpl svcImpl = locator.getService(AppsServiceImpl.class);
+//
+//    // Call the main service init method
+//    svcImpl.initService(siteId, siteAdminTenantId, RuntimeParameters.getInstance().getServicePassword());
+//    // Create and start the server
+//    final HttpServer server = GrizzlyHttpServerFactory.createHttpServer(baseUri, config, false);
+//    server.start();
+
+    final HttpServer server = GrizzlyHttpServerFactory.createHttpServer(baseUri, config, false);
+    Collection<NetworkListener> listeners = server.getListeners();
+    for (NetworkListener listener : listeners)
+    {
+      System.out.println("**** Listener: " + listener.toString() + " ****");
+      final TCPNIOTransport transport = listener.getTransport();
+      transport.setKeepAlive(true);
+      transport.setWriteTimeout(0, TimeUnit.MINUTES);
+      final ThreadPoolConfig tpc = transport.getWorkerThreadPoolConfig();
+      tpc.setQueueLimit(-1).setCorePoolSize(100).setMaxPoolSize(100);
+    }
+    server.start();
   }
 }
