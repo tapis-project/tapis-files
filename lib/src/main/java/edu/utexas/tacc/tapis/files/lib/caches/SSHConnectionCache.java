1d9e816759d993d8144b9c9b1e2b0342a5274089
Steve Black <scblack@tacc.utexas.edu>
2021-12-01 09:21:46 -0600

    Update SSH conn log msg, docker build update. Ops tests pass.

--- lib/src/main/java/edu/utexas/tacc/tapis/files/lib/caches/SSHConnectionCache.java
+++ lib/src/main/java/edu/utexas/tacc/tapis/files/lib/caches/SSHConnectionCache.java
@@ -3,6 +3,7 @@ package edu.utexas.tacc.tapis.files.lib.caches;
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheStats;
 import com.google.common.cache.LoadingCache;
+import edu.utexas.tacc.tapis.files.lib.utils.Utils;
 import edu.utexas.tacc.tapis.systems.client.gen.model.TapisSystem;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,35 +14,34 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
-
 /**
  * The SSHConnectionCache stores SSH connections in a Guava LoadingCache which is periodically
  * checked to see if the SSH sessions are active or not. If the session is not active, it
- * is disconnected and removed from the cache. THe next get() operation on the cache will reinstantiate
- * the connection and place it back in the cache.
+ * is disconnected and removed from the cache. THe next get() operation on the cache will create a new
+ * connection and place it back in the cache.
  **
  * A single threaded ScheduledExecutorService is used to periodically do the maintenance
  *
  * The cache key is a combination of systemId, tenant and username
  */
-public class SSHConnectionCache {
-
+public class SSHConnectionCache
+{
     private static final Logger log = LoggerFactory.getLogger(SSHConnectionCache.class);
     private static LoadingCache<SSHConnectionCacheKey, SSHConnectionHolder> sessionCache;
     private  final ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
     private final TimeUnit timeUnit;
     private final long timeout;
+
     /**
      *
      * @param timeout Timeout of when to preform maintenance
      * @param timeUnit TimeUnit of timeout
      */
-    public SSHConnectionCache(long timeout, TimeUnit timeUnit) {
+    public SSHConnectionCache(long timeout, TimeUnit timeUnit)
+    {
         this.timeUnit = timeUnit;
         this.timeout = timeout;
-        sessionCache = CacheBuilder.newBuilder()
-                .recordStats()
-                .build(new SSHConnectionCacheLoader());
+        sessionCache = CacheBuilder.newBuilder().recordStats().build(new SSHConnectionCacheLoader());
         executorService.scheduleAtFixedRate( ()-> {
             sessionCache.asMap().forEach( (SSHConnectionCacheKey key, SSHConnectionHolder holder) -> {
                 if (holder.getChannelCount() == 0) {
@@ -50,9 +50,8 @@ public class SSHConnectionCache {
                     holder.getSshConnection().stop();
                 }
             });
-        }, this.timeout, this.timeout, this.timeUnit); //
+        }, this.timeout, this.timeout, this.timeUnit);
     }
-
 
     /**
      *
@@ -70,21 +69,23 @@ public class SSHConnectionCache {
         return sessionCache;
     }
 
-
     /**
      *
      * @param system System object
-     * @param username API username
+     * @param oboUser API username
      * @return will return a SSH connection, either directly from the cache (if it exists), or
      * create one and return it from the cache.
      * @throws IOException
      */
-    public SSHConnectionHolder getConnection(TapisSystem system, String username) throws IOException {
-        SSHConnectionCacheKey key = new SSHConnectionCacheKey(system, username);
-        try {
-            return sessionCache.get(key);
-        } catch (ExecutionException ex) {
-            log.error("SSHSessionCache get error", ex);
+    public SSHConnectionHolder getConnection(TapisSystem system, String oboUser) throws IOException
+    {
+        SSHConnectionCacheKey key = new SSHConnectionCacheKey(system, oboUser);
+        try { return sessionCache.get(key); }
+        catch (ExecutionException ex)
+        {
+          String msg = Utils.getMsg("FILES_CLIENT_SSH_CONN_ERR", system.getTenant(), oboUser, system.getId(), oboUser,
+                                    system.getHost(), ex.getMessage());
+            log.error(msg, ex);
             throw new IOException("Could not get or create SSH session");
         }
     }
