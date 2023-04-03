92fae9a26664e2e68f2716420d8942fac1abb760
Steve Black <scblack@tacc.utexas.edu>
2023-04-03 09:05:50 -0500

    globus: minor updates.

--- migrations/src/main/resources/db/migration/V008__addExternalTaskId.sql
+++ migrations/src/main/resources/db/migration/V008__addExternalTaskId.sql
@@ -1,4 +1,4 @@
 -- ------------------------------------------------------------------------------------------------------
 -- Add column for child transfer task external txfr id.
 -- ------------------------------------------------------------------------------------------------------
-ALTER TABLE transfer_tasks_child ADD COLUMN IF NOT EXISTS external_task_id TEXT NOT NULL DEFAULT "";
+ALTER TABLE transfer_tasks_child ADD COLUMN IF NOT EXISTS external_task_id TEXT NOT NULL;
