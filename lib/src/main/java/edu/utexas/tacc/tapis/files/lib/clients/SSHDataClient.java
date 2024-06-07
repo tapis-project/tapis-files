1a6ebefb6f19cd9e9db60af9f70add70be059d5e
Dan Vernon <dvernon@tacc.utexas.edu>
2024-06-07 10:24:13 -0500

    remove unused import

--- lib/src/main/java/edu/utexas/tacc/tapis/files/lib/clients/SSHDataClient.java
+++ lib/src/main/java/edu/utexas/tacc/tapis/files/lib/clients/SSHDataClient.java
@@ -4,7 +4,6 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
