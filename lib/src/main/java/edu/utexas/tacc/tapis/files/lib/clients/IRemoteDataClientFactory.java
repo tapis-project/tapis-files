a06b1545835038d489aa74bda35ed828d9b5bfa6
Dan Vernon <dvernon@tacc.utexas.edu>
2024-08-01 14:50:27 -0500

    Bug fix for sharedCtxGrantor in transfers and errant eol character (\n) in build time stamp of response messages

--- lib/src/main/java/edu/utexas/tacc/tapis/files/lib/clients/IRemoteDataClientFactory.java
+++ lib/src/main/java/edu/utexas/tacc/tapis/files/lib/clients/IRemoteDataClientFactory.java
@@ -6,7 +6,13 @@ import edu.utexas.tacc.tapis.systems.client.gen.model.TapisSystem;
 
 public interface IRemoteDataClientFactory
 {
+   // Some methods do not support impersonationId or sharedAppCtxGrantor
+   public static final String IMPERSONATION_ID_NULL = null;
+   public static final String SHARED_CTX_GRANTOR_NULL = null;
+
+
    IRemoteDataClient getRemoteDataClient(@NotNull String oboTenant, @NotNull String oboUser,
-                                         @NotNull TapisSystem system)
-           throws IOException;
+                                         @NotNull TapisSystem system, String impersonationId,
+                                         String sharedCtxGrantor)
+   throws IOException;
 }
