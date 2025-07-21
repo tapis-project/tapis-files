0fa1a03a953d137516763720d20b3d6d3fec9546
Dan Vernon <dvernon@tacc.utexas.edu>
2025-07-21 10:34:21 -0500

    Adding workflows to trusted services for files

--- api/src/main/java/edu/utexas/tacc/tapis/files/api/FilesApplication.java
+++ api/src/main/java/edu/utexas/tacc/tapis/files/api/FilesApplication.java
@@ -94,7 +94,7 @@ public class FilesApplication extends ResourceConfig
   // We must be running on a specific site and this will never change
   private static String siteId;
   public static String getSiteId() {return siteId;}
-  public static List<String> getTrustedServices() {return List.of(TapisConstants.SERVICE_NAME_JOBS);}
+  public static List<String> getTrustedServices() {return List.of(TapisConstants.SERVICE_NAME_JOBS, "workflows");}
   private static String siteAdminTenantId;
   public static String getSiteAdminTenantId() {return siteAdminTenantId;}
 
