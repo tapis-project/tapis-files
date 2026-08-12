94b93ea61d4ab6925412b9b4a0b7dff2da220381
Dan Vernon <dvernon@tacc.utexas.edu>
2026-08-12 17:31:40 -0500

    Fixing error where getFileInfo after mkdir was using the wrong path

--- lib/src/main/java/edu/utexas/tacc/tapis/files/lib/clients/SSHDataClient.java
+++ lib/src/main/java/edu/utexas/tacc/tapis/files/lib/clients/SSHDataClient.java
@@ -307,8 +307,10 @@ public class SSHDataClient implements ISSHDataClient, ArchiveTransferSource, Arc
           //
           // UPDATE:  Adding 10X loop in the hopes it will help on the actual HPC machines.  Sometimes we're still
           // seeing failures on mkdir of deep directory structures during transfers.  This is a concurrency issue.
+          //
+          // UPDATE 2:  It turns out the getFileInfo was using the wrong path!!  This code may get a WHOLE lot simpler
+          // if this works out.  We can probably remove the loop with sleep if we don't need it after this fix
           for(counter=1;counter<=20;counter++) {
-            Thread.sleep(100);
             // Caught an exception.  If we look and see a directory there, it most likely means it was created
             // by another thread.  That's fine.  It's been created, so we will call it a success.  Unfortunately
             // getFileInfo requires a relative path though, and not an absolute path, so make the conversion first.
@@ -320,6 +322,7 @@ public class SSHDataClient implements ISSHDataClient, ArchiveTransferSource, Arc
               break;
             }
             log.warn(String.format("MKDIR did not see directory after %d iterations of checking. Path: %s", counter, tmpPathStr));
+            Thread.sleep(100);
           }
         } catch (Exception ex) {
           // ignore this - it's handled below with the original exception
